10acd8010ba7e9680cad0016690d90ced40c9be2
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-17 22:49:30 +0530

    Practice

---
+++ q3.js
@@ -0,0 +1,9 @@
+console.log(10 + 5); // Ouput will be 15
+console.log("10" + 5); //Error it will come as cant add two different data types (expected)
+console.log("10" - 5); // Same as before (expected)
+console.log("10" * 5); // Same as before (expected)
+console.log(10 === "10"); // Value is same but datatype is diff so false is output
+console.log(10 == "10"); // Output will be true
+
+// For the 2nd,3rd and 4th lines of code, it actually doesnt give error and give output as javascript has smth called as
+// Type corceion and it automatically converts the datatype to make the code work!!
