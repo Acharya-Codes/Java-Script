13e499b920f763297722fa8fc493e59714d0ef4b
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-17 22:27:48 +0530

    Learning comparison operators

--- compare.js
+++ compare.js
@@ -4,3 +4,4 @@ console.log(age > 18);
 console.log(age < 18);
 console.log(age == 18);
 console.log(age != 18);
+console.log(5 === "5"); // This command checks both the type and the value, in this case value is the same but type is different so it will return false!!
