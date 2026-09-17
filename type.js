40ec11a03e4d5463bb95e0b983a769a6491dac95
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-17 22:18:20 +0530

    Learning typeof func

---
+++ type.js
@@ -0,0 +1,8 @@
+const name = "Acharya";
+const age = 17;
+const course = "CSE";
+const is_student = true;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof course);
+console.log(typeof is_student);
