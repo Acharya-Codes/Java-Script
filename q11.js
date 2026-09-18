9b7233505254a369f95667ea3100d67260dd43a1
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-18 08:02:02 +0530

    Practice

---
+++ q11.js
@@ -0,0 +1,7 @@
+const isAdmin = true;
+const isProffesor = false;
+if(isAdmin || isProffesor) {
+    console.log("You can enter the room");
+}else {
+    console.log("You cannot enter the room");
+}
