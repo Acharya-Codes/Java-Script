fcf1c3f1c3e5677e3925e7666a994f1841059f7a
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-18 07:35:21 +0530

    Practice

---
+++ q9.js
@@ -0,0 +1,14 @@
+const marks = 87;
+if(marks > 90) {
+    console.log("A+");
+}else if(80 <= marks <= 89) {
+    console.log("A");
+}else if(70 <= marks <= 79) {
+    console.log("B");
+}else if(60 <= marks <= 69) {
+    console.log("C");
+}else if(50 <= marks <= 59) {
+    console.log("D");
+}else {
+    console.log("F");
+}
