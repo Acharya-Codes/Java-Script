49082800e4da9b6d79ef598e369158ba199d8d09
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-18 10:13:01 +0530

    Practice

---
+++ q15.js
@@ -0,0 +1,6 @@
+console.log("Acharya" || "Rahul");
+console.log("" || "Rahul");
+console.log("Acharya" && "Rahul");
+console.log("" && "Rahul");
+// OR --> Give me the first thing which is usable!
+// AND --> Give me the first unusable, otherwise the last thing!
