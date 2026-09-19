799f197eee250a12d0b8a5cdd4276c17a234797c
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-19 08:31:19 +0530

    Learnt arrow functions

---
+++ arrow.js
@@ -0,0 +1,6 @@
+const sum = (a,b) => a+b; // If two parameters there then paranthesis is needed
+const greet = () => {
+    console.log("Hello"); // If emopty then also paranthesis needed
+}
+greet();
+const sq = a => a*a; // If only one parameter no paranthesis needed
