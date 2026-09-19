12e46bb54b41f1638f28c12081f175fa52443801
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-19 18:25:29 +0530

    Learnt maps

--- maps.js
+++ maps.js
@@ -3,4 +3,5 @@ const result = num.map(function(n) {
     return n*2;
 });
 console.log(result);
-// Basically take an array, applies the given condition and then applied the condition onto the array!
+// Basically take an array, applies the given condition and then applied the condition onto the array! 
+// These changes will be visible in the new array which it created and the old one will be unchanged
