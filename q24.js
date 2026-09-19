ff09b7f1c672bf24edbcf3ee094f6458580c1b8d
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-19 08:46:41 +0530

    Practice

---
+++ q24.js
@@ -0,0 +1,10 @@
+const isPositive = n => {
+    if(n>0) {
+        console.log("Positive gng");
+    }else if(n===0) {
+        console.log("Zero gng");
+    }else {
+        console.log("Negative gng");
+    }
+}
+isPositive(4);
