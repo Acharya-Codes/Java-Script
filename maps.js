1654960016dc1cbad5990f7515526ab6f2b0f326
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-19 18:23:34 +0530

    Learnt maps

---
+++ maps.js
@@ -0,0 +1,6 @@
+const num = [1,2,3,4,5];
+const result = num.map(function(n) {
+    return n*2;
+});
+console.log(result);
+// Basically take an array, applies the given condition and then applied the condition onto the array!
