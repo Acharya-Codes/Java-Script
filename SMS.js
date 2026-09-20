feabac996057db5f33069227a5b6cc04a407c9fa
Acharya-Codes <acharyan7a1@gmail.com>
2026-09-20 10:26:21 +0530

    Learnt rest

---
+++ SMS.js
@@ -0,0 +1,56 @@
+const students = [
+    {
+        name: "A",
+        marks: 85,
+        course: "CSE",
+        skills: ["C", "Python"]
+    },
+    {
+        name: "B",
+        marks: 72,
+        course: "CSE",
+        skills: ["Java", "Python"]
+    },
+    {
+        name: "C",
+        marks: 91,
+        course: "ECE",
+        skills: ["C++", "Python", "MATLAB"]
+    },
+    {
+        name: "D",
+        marks: 64,
+        course: "CSE",
+        skills: ["C", "JavaScript"]
+    },
+    {
+        name: "E",
+        marks: 88,
+        course: "CSE",
+        skills: ["Python", "JavaScript"]
+    }
+];
+const topStudents = students.filter(student => student.marks >= 80);
+const topStudentsName = topStudents.map(student => student.name);
+const CSE = topStudents.filter(student => student.course = "CSE");
+const firstTopCSE = CSE.find(student => student.marks >= 80);
+const needStudent = students.find(student => student.name === "A");
+const updatedStudent = {
+    ...needStudent,
+    marks: 95,
+}
+function getFirstSkill(skills) {
+    skills = ["Python","C","C++"];
+    const [first,...rest] = skills;
+    console.log(first);
+}
+
+
+
+
+
+console.log(topStudents);
+console.log(topStudentsName);
+console.log(firstTopCSE);
+console.log(updatedStudent);
+getFirstSkill(["Java","Python"]);
--- rest.js
+++ rest.js
@@ -1,7 +1,4 @@
 const num = [1,2,3,4,5];
-const [first,...rest] = num;
-console.log(first);
-console.log(rest);
 const [first,second,...remaining] = num;
 console.log(first);
 console.log(second);
